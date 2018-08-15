ffd2f6002df3a0094a9fbd57a46506fef444b71c
Bansole <41703901+Bansole@users.noreply.github.com>
2018-08-15 17:44:29 +0530

    Add files via upload
    added uart transmit,init and receive function

--- io.h
+++ io.h
@@ -68,6 +68,35 @@ void (*cAllisr)(void);		//function pointer used in ISR()
 
  
 
+void USART_Init( unsigned int ubrr)
+{
+	/*Set baud rate */
+	UBRRH = (unsigned char)(ubrr>>8);
+	UBRRL = (unsigned char)ubrr;
+	/*Enable receiver and transmitter */
+UCSRB = (1<<RXEN)|(1<<TXEN);
+
+}
+/* Set frame format: 8data, 2stop bit */
+void USART_Transmit( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSRA & (1<<UDRE)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR = data;
+	_delay_ms(100);
+	
+	
+}
+unsigned char USART_Receive( void )
+{
+/* Wait for data to be received */
+while ( !(UCSRA & (1<<RXC)) )
+;
+/* Get and return received data from buffer */
+return UDR;}
+
 
 
 void Dmilli(int j)
