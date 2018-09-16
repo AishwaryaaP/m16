5ccbe8ddb4d992506f083b292abd49be2b22a8fc
Shaunak9899 <43040823+Shaunak9899@users.noreply.github.com>
2018-09-16 05:36:22 -0700

    Added pinMode,digitalWrite etc functions and did the pin mapping.

--- io.h
+++ io.h
@@ -44,6 +44,11 @@
 #include <stdlib.h>
 #include<math.h>
 
+void pinMode(uint8_t , uint8_t );
+static void turnOffPWM(uint8_t );
+void digitalWrite(uint8_t , uint8_t );
+int digitalRead(uint8_t );
+
 void delay(unsigned long);		//simpler form of delay function in avr
 void analogWrite(uint8_t, uint8_t);		//PWM function using TIMER 0   ***NOTE:Timer 0 cannot be used if this function is used in the code***
 uint16_t analogRead(uint8_t);
@@ -65,6 +70,208 @@ const uint8_t A=1,B=2,C=3,D=4,lowerNibble=8,higherNibble=9,ALL=10,D4=4,D5=5;
 /***VARIABLES lowerNibble, higherNibble, ALL ARE FOR SETTING A SET OF BIT AT ONCE***/
 void (*cAllisr)(void);		//function pointer used in ISR()
 void (*uSerfun) (void);        //function pointer used inISR() of softwareInterrupt
+
+
+
+
+
+void pinMode(uint8_t pIn, uint8_t mOde)
+{
+	uint8_t bit = digitalPinToBitMask(pIn);
+	uint8_t port = digitalPinToPort(pIn);
+	volatile uint8_t *reg, *out;
+
+	if (port == NOT_A_PIN) return;
+
+	// JWS: can I let the optimizer do this?
+	reg = portModeRegister(port);
+	out = portOutputRegister(port);
+
+	if (mOde == INPUT) { 
+		uint8_t oldSREG = SREG;
+                cli();
+		*reg &= ~bit;
+		*out &= ~bit;
+		SREG = oldSREG;
+	} else if (mOde == INPUT_PULLUP) {
+		uint8_t oldSREG = SREG;
+                cli();
+		*reg &= ~bit;
+		*out |= bit;
+		SREG = oldSREG;
+	} else {
+		uint8_t oldSREG = SREG;
+                cli();
+		*reg |= bit;
+		SREG = oldSREG;
+	}
+}
+
+
+static void turnOffPWM(uint8_t tImer)
+{
+	switch (tImer)
+	{
+		#if defined(TCCR1A) && defined(COM1A1)
+		case TIMER1A:   cbi(TCCR1A, COM1A1);    break;
+		#endif
+		#if defined(TCCR1A) && defined(COM1B1)
+		case TIMER1B:   cbi(TCCR1A, COM1B1);    break;
+		#endif
+		#if defined(TCCR1A) && defined(COM1C1)
+		case TIMER1C:   cbi(TCCR1A, COM1C1);    break;
+		#endif
+		
+		#if defined(TCCR2) && defined(COM21)
+		case  TIMER2:   cbi(TCCR2, COM21);      break;
+		#endif
+		
+		#if defined(TCCR0A) && defined(COM0A1)
+		case  TIMER0A:  cbi(TCCR0A, COM0A1);    break;
+		#endif
+		
+		#if defined(TCCR0A) && defined(COM0B1)
+		case  TIMER0B:  cbi(TCCR0A, COM0B1);    break;
+		#endif
+		#if defined(TCCR2A) && defined(COM2A1)
+		case  TIMER2A:  cbi(TCCR2A, COM2A1);    break;
+		#endif
+		#if defined(TCCR2A) && defined(COM2B1)
+		case  TIMER2B:  cbi(TCCR2A, COM2B1);    break;
+		#endif
+		
+		#if defined(TCCR3A) && defined(COM3A1)
+		case  TIMER3A:  cbi(TCCR3A, COM3A1);    break;
+		#endif
+		#if defined(TCCR3A) && defined(COM3B1)
+		case  TIMER3B:  cbi(TCCR3A, COM3B1);    break;
+		#endif
+		#if defined(TCCR3A) && defined(COM3C1)
+		case  TIMER3C:  cbi(TCCR3A, COM3C1);    break;
+		#endif
+
+		#if defined(TCCR4A) && defined(COM4A1)
+		case  TIMER4A:  cbi(TCCR4A, COM4A1);    break;
+		#endif					
+		#if defined(TCCR4A) && defined(COM4B1)
+		case  TIMER4B:  cbi(TCCR4A, COM4B1);    break;
+		#endif
+		#if defined(TCCR4A) && defined(COM4C1)
+		case  TIMER4C:  cbi(TCCR4A, COM4C1);    break;
+		#endif			
+		#if defined(TCCR4C) && defined(COM4D1)
+		case TIMER4D:	cbi(TCCR4C, COM4D1);	break;
+		#endif			
+			
+		#if defined(TCCR5A)
+		case  TIMER5A:  cbi(TCCR5A, COM5A1);    break;
+		case  TIMER5B:  cbi(TCCR5A, COM5B1);    break;
+		case  TIMER5C:  cbi(TCCR5A, COM5C1);    break;
+		#endif
+	}
+}
+
+void digitalWrite(uint8_t pIn, uint8_t val)
+{
+	uint8_t tImer = digitalPinToTimer(pIn);
+	uint8_t bit = digitalPinToBitMask(pIn);
+	uint8_t port = digitalPinToPort(pIn);
+	volatile uint8_t *out;
+
+	if (port == NOT_A_PIN) return;
+
+	// If the pin that support PWM output, we need to turn it off
+	// before doing a digital write.
+	if (tImer != NOT_ON_TIMER) turnOffPWM(tImer);
+
+	out = portOutputRegister(port);
+
+	uint8_t oldSREG = SREG;
+	cli();
+
+	if (val == LOW) {
+		*out &= ~bit;
+	} else {
+		*out |= bit;
+	}
+
+	SREG = oldSREG;
+}
+
+int digitalRead(uint8_t pIn)
+{
+	uint8_t tImer = digitalPinToTimer(pIn);
+	uint8_t bit = digitalPinToBitMask(pIn);
+	uint8_t port = digitalPinToPort(pIn);
+
+	if (port == NOT_A_PIN) return LOW;
+
+	// If the pin that support PWM output, we need to turn it off
+	// before getting a digital reading.
+	if (timer != NOT_ON_TIMER) turnOffPWM(tImer);
+
+	if (*portInputRegister(port) & bit) return HIGH;
+	return LOW;
+}
+
+const uint8_t PROGMEM digital_pin_to_port_PGM[] ={
+	
+PB, //(XCK/T0)
+PB,//(T1)
+PB,//(INT2/AIN0)
+PB, //(OC0/AIN1)
+PB, //(SS) 
+PB, //(MOSI) 
+PB, //(MISO)
+PB,//(SCK)
+PD, //(RXD)
+PD, //(TXD)
+PD,//(INT0) 
+PD,//(INT1)
+PD,//(OC1B)
+PD,//(OC1A)
+PD,	//(ICP)
+PD, //(OC2)
+PC,//(SCL)
+PC, //(SDA)
+PC, //(TCK)
+PC, //(TMS)
+PC, //(TDO)
+PC, //(TDI)
+PC, //(TOSC1)
+PC,// (TOSC2)
+
+};
+
+const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
+	
+_BV(0), //(XCK/T0)
+_BV(1),//(T1)
+_BV(2),//(INT2/AIN0)
+_BV(3), //(OC0/AIN1)
+_BV(4), //(SS) 
+_BV(5), //(MOSI) 
+_BV(6), //(MISO)
+_BV(7),//(SCK)
+_BV(0), //(RXD)
+_BV(1), //(TXD)
+_BV(2),//(INT0) 
+_BV(3),//(INT1)
+_BV(4),//(OC1B)
+_BV(5),//(OC1A)
+_BV(6),	//(ICP)
+_BV(7), //(OC2)
+_BV(0), //(SCL)
+_BV(1), //(SDA)
+_BV(2), //(TCK)
+_BV(3), //(TMS)
+_BV(4), //(TDO)
+_BV(5), //(TDI)
+_BV(6), //(TOSC1)
+_BV(7),// (TOSC2)
+
+
+};
 
 double microsecondsToInches(unsigned long mIcroseconds) {
   // According to Parallax's datasheet for the PING))), there are 73.746
